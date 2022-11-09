191da7918487c543ccc97865320e8f3b53b914b6
GEPOC <webgepoc@gmail.com>
2022-11-09 11:11:30 +0100

    removed common cols from preprocessing

--- clasificadorPCALDAmeteoNdays.py
+++ clasificadorPCALDAmeteoNdays.py
@@ -13,46 +13,41 @@ import sklearn.metrics as skmetrics
 import sklearn.decomposition as skdecomp
 import isadoralib as isl
 ndias=5
+csv_folder="ignore/xycsv/"
 year_train="2014"
 year_test="2015"
 year_data="2019"
-sufix="rht"
-comp=4
-fltp=4
-fmeteo=4
-saveFolder="ignore/figures/PCALDAMETEOresults"+str(ndias)+"/"+year_train+"-"+year_data+"-"+year_test+"-"+sufix+"/"
+# sufix="rht"
+# comp=4
+# fltp=4
+# fmeteo=4
+saveFolder="ignore/figures/PCALDAMETEOresults"+str(ndias)+"/"+year_train+"-"+year_data+"-"+year_test+"/"
 n_dias=5
 #matplotlib.use("Agg")
 
 # Carga de datos de entrenamiento
-tdvT,ltpT,meteoT,trdatapd=isl.cargaDatos(year_train,sufix)
+xtr=pd.read_csv(csv_folder+"x"+year_train+".csv",index_col=[0,1],header=[0,1])
+xtr.index = xtr.index.set_levels(pd.to_timedelta(xtr.index.levels[1]), level=1)
+xtr.columns = xtr.columns.set_levels(pd.to_datetime(xtr.columns.levels[1]), level=1)
+ytr=pd.read_csv(csv_folder+"y"+year_train+".csv",index_col=[0,1])
 
 # Carga de datos de validación
-tdvP,ltpP,meteoP,valdatapd=isl.cargaDatos(year_data,sufix)
+xv=pd.read_csv(csv_folder+"x"+year_data+".csv",index_col=[0,1],header=[0,1])
+xv.index = xv.index.set_levels(pd.to_timedelta(xv.index.levels[1]), level=1)
+xv.columns = xv.columns.set_levels(pd.to_datetime(xv.columns.levels[1]), level=1)
+yv=pd.read_csv(csv_folder+"y"+year_data+".csv",index_col=[0,1])
 
 # Carga de datos de test
-tdvTs,ltpTs,meteoTs,tsdatapd=isl.cargaDatos(year_test,sufix)
+xts=pd.read_csv(csv_folder+"x"+year_test+".csv",index_col=[0,1],header=[0,1])
+xts.index = xts.index.set_levels(pd.to_timedelta(xts.index.levels[1]), level=1)
+xts.columns = xts.columns.set_levels(pd.to_datetime(xts.columns.levels[1]), level=1)
+yts=pd.read_csv(csv_folder+"y"+year_test+".csv",index_col=[0,1])
+#xtr.index = xtr.index.set_levels(pd.to_timedelta(xtr.index.levels[1])+pd.Timedelta(1,"day"), level=1)
+print(xtr)
 
-# guarda la información raw para plots
-ltpPlot = ltpP.copy()
-meteoPlot = meteoP.copy()
-
-# aplica un filtro de media móvil a ltp
-ltpT = ltpT.rolling(window=240,center=True).mean()
-ltpP = ltpP.rolling(window=240,center=True).mean()
-ltpTs = ltpTs.rolling(window=240,center=True).mean()
-
-#interpola los datos meteo
-meteoT=meteoT.resample('1T').interpolate('linear')
-meteoP=meteoP.resample('1T').interpolate('linear')
-meteoTs=meteoTs.resample('1T').interpolate('linear')
-
-xt,yt=isl.datosADataframe(ltpT,meteoT,trdatapd)
-xp,yp=isl.datosADataframe(ltpP,meteoP,valdatapd)
-xts,yts=isl.datosADataframe(ltpTs,meteoTs,tsdatapd)
-
-
-
-print(xp)
-print(valdatapd)
-sys.exit()
+xtrn=xtr.copy()
+for i in range(ndias+1):
+    xtrn.index=xtrn.index.set_levels(xtrn.index.levels[1]-pd.Timedelta(1,"day"), level=1)
+    
+    xtrn=pd.concat([xtrn,xtr])
+print(xtrn)
