4ab1404f2501cd404c9a38a987ddcd4cff83c941
GEPOC <webgepoc@gmail.com>
2023-08-10 23:28:09 +0200

    improved month data representation

--- graficaMesTDVColor.py
+++ graficaMesTDVColor.py
@@ -22,30 +22,81 @@ matplotlib.use('Agg')
 #years_test = ["2015"]
 years_test = ["2014","2015","2016","2019"]
               
-save_folder = 'ignore/resultadosTDV/batch/PCALDA6am/IMG/OriginalRiegoDatosNuevosColor/'
+save_folder = 'ignore/resultadosTDV/batch/GraficaMes/'
 if not os.path.exists(save_folder):
     os.makedirs(save_folder)
 
-#crea una carpeta para guardar los resultados si no existe por cada año de test
+#create a folder to save the results if it doesn't exist for each test year
 for year_test in years_test:
     if not os.path.exists(save_folder+year_test):
         os.makedirs(save_folder+year_test)
 
 
 
-#crea dos listas vacías para los datos de test
+#create three empty lists for the data: tdv, data, and sun
 tdv_tests=[]
 data_tests=[]
-#por cada año de test 
+sun_tests=[]
+
+# for each test year
 for year_test in years_test:
 
     if not os.path.isfile("rawDiarios"+year_test+".csv") or not os.path.isfile("rawMinutales"+year_test+".csv"):
         os.system("python3 cargaRaw.py")
-    #carga los datos de test
+    #load the test data
     tdv_test,ltp_test,meteo_test,data_test=isl.cargaDatosTDV(year_test,"rht")
-    #añade los datos de test a las listas
+
+    #from meteo, only keep radiation info (R_Neta_Avg)
+    meteo_test=meteo_test["R_Neta_Avg"]
+
+    #create a rad_sign dataframe with value 1 where meteo is positive and -1 where meteo is negative
+    rad_sign=meteo_test.copy()
+    rad_sign[rad_sign>0]=1
+    rad_sign[rad_sign<0]=-1
+    #create a rad_sign_diff dataframe with the difference between consecutive values of rad_sign
+    rad_sign_diff=rad_sign.diff()
+    #remove zero values from rad_sign_diff
+    rad_sign_diff=rad_sign_diff[rad_sign_diff!=0]
+    #remove nan values from rad_sign_diff
+    rad_sign_diff=rad_sign_diff.dropna()
+    #separate the positive and negative values of rad_sign_diff
+    rad_sign_diff_pos=rad_sign_diff[rad_sign_diff>0]
+    rad_sign_diff_neg=rad_sign_diff[rad_sign_diff<0]
+    # separate date and hour from the index of rad_sign_diff_pos into two columns in a new dataframe
+    sun=pd.DataFrame()
+    #get date
+    sun["date"]=rad_sign_diff_pos.index.date
+    #get hour format hh:mm:ss
+    sun["sunrise"]=rad_sign_diff_pos.index.time
+    # make date the index of sun
+    sun=sun.set_index("date")
+    # remove duplicate dates keeping the first one
+    sun=sun[~sun.index.duplicated(keep='first')]
+    # separate date and hour from the index of rad_sign_diff_neg into two columns in a new dataframe
+    sunAux=pd.DataFrame()
+    #get date
+    sunAux["date"]=rad_sign_diff_neg.index.date
+    #get hour format hh:mm:ss
+    sunAux["sunset"]=rad_sign_diff_neg.index.time
+    # make date the index of sunAux
+    sunAux=sunAux.set_index("date")
+    # remove duplicate dates keeping the last one
+    sunAux=sunAux[~sunAux.index.duplicated(keep='last')]
+    # merge sun and sunAux into a single dataframe
+    sun=sun.merge(sunAux,how="outer",left_index=True,right_index=True)
+    # add a column with next day's sunrise
+    sun["sunriseNext"]=sun["sunrise"].shift(-1)
+
+    # convert sun index to datetime
+    sun.index=pd.to_datetime(sun.index)
+
+    # remove any row with a nan value
+    sun=sun.dropna()
+
+    #add the test data to the lists
     tdv_tests.append(tdv_test.copy())
     data_tests.append(data_test.copy())
+    sun_tests.append(sun.copy())
 
 
 # por cada elemento de tdv_tests
@@ -108,21 +159,34 @@ for i in range(len(tdv_tests)):
             # bottom, top = plt.ylim()
             # plt.ylim(bottom-(top-bottom)*0.1,top)
 
-            # obtiene los índices de la columna j del mes month en los que el día es par y la hora las 00:00:00
-            days=tdv_test_month.loc[(tdv_test_month.index.day%2==0) & (tdv_test_month.index.time==time(0,0,0))].index
-            # por cada día par
-            for day in days:
-                # crea un área de color gris
-                plt.axvspan(day,day+pd.Timedelta(days=1),color='gray',alpha=0.1)
+            # # obtiene los índices de la columna j del mes month en los que el día es par y la hora las 00:00:00
+            # days=tdv_test_month.loc[(tdv_test_month.index.day%2==0) & (tdv_test_month.index.time==time(0,0,0))].index
+            # # por cada día par
+            # for day in days:
+            #     # crea un área de color gris
+            #     plt.axvspan(day,day+pd.Timedelta(days=1),color='gray',alpha=0.1)
 
 
-            # obtiene los índices de los datos de test de la columna j del mes month
+            # get the indices of the test data of column j of month month
             days=data_tests[i].loc[(data_tests[i].index.month==month)].index
 
-            # por cada día
+            # for each day
             for day in days:
-                #añade un área de color verde entre las 5:00 y las 8:00
+                #add a green area between 5:00 and 8:00
                 plt.axvspan(day+pd.Timedelta(hours=5),day+pd.Timedelta(hours=8),color='green',alpha=0.1)
+            
+            #get the indices of the sun data of month month
+            days=sun_tests[i].loc[sun_tests[i].index.month==month].index
+
+            #for each day
+            for day in days:
+                #get the sunrise and sunset times
+                sunrise=sun_tests[i].loc[day,"sunrise"]
+                sunset=sun_tests[i].loc[day,"sunset"]
+                #add a gray area between 00:00 and sunrise
+                plt.axvspan(day,day+pd.Timedelta(hours=sunrise.hour,minutes=sunrise.minute,seconds=sunrise.second),color='gray',alpha=0.1)
+                #add a gray area between sunset and 00:00 of the next day
+                plt.axvspan(day+pd.Timedelta(hours=sunset.hour,minutes=sunset.minute,seconds=sunset.second),day+pd.Timedelta(days=1),color='gray',alpha=0.1)
 
             # obtiene los índices de data_tests
             days=data_tests[i].loc[data_tests[i].index.month==month].index
