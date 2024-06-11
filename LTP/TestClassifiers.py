63f82f0433ca263c7d388aa079983a3f2a8e2c65
GEPOC <webgepoc@gmail.com>
2024-06-11 20:05:17 +0200

    test classifiers

---
+++ LTP/TestClassifiers.py
@@ -0,0 +1,171 @@
+import pandas as pd
+import numpy as np
+from datetime import time
+import os
+import sys
+import matplotlib.pyplot as plt
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+
+# ZIM data file in db/ZIMdb14151619Meteo.csv of root directory
+dataFile='db/ZIMdb14151619Meteo.csv'
+
+# Report file in results/ZIM/ of root directory, the name of the file is the name of the classifier
+reportFolder='results/ZIM/'
+
+# years to be used as training data
+year_train=['2014']
+
+# classifiers to be used
+classifiers = [
+    LinearDiscriminantAnalysis(),
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025, random_state=42),
+    SVC(gamma=2, C=1, random_state=42),
+    GaussianProcessClassifier(1.0 * RBF(1.0), random_state=42),
+    DecisionTreeClassifier(max_depth=5, random_state=42),
+    RandomForestClassifier(
+        max_depth=5, n_estimators=10, max_features=1, random_state=42
+    ),
+    MLPClassifier(alpha=1, max_iter=1000, random_state=42),
+    AdaBoostClassifier(algorithm="SAMME", random_state=42),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis(),
+]
+# classifier labels
+clf_labels = [
+    "LDA",
+    "Nearest Neighbors",
+    "Linear SVM",
+    "RBF SVM",
+    "Gaussian Process",
+    "Decision Tree",
+    "Random Forest",
+    "Neural Net",
+    "AdaBoost",
+    "Naive Bayes",
+    "QDA",
+]
+
+# best accuracy obtained
+bestacc=0
+# best classifier
+bestclf=''
+
+#list of accuracies
+accs=[]
+
+# Read ZIM data file. use the first two columns as index
+data = pd.read_csv(dataFile, sep=',', decimal='.', index_col=[0,1])
+
+# # 'Fecha' (second index column) is a string with the format 'YYYY-MM-DD', convert it to datetime
+# data.index.set_levels(pd.to_datetime(data.index.levels[1]), level=1)
+# print(data.head())
+
+# get the year of each data from the second index (yyyy-mm-dd) splitting by "-"
+data["year"]=data.index.get_level_values(1).str.split("-").str[0]
+
+# select the training data as the data in the year_train list
+dbtrain=data.loc[data["year"].isin(year_train)].copy()
+# drop the year column
+dbtrain.drop("year", axis=1, inplace=True)
+
+# extract the y column
+ytrain=dbtrain["Y"]
+# drop the y column
+dbtrain.drop("Y", axis=1, inplace=True)
+
+# create the report folder if it does not exist
+if not os.path.exists(reportFolder):
+    os.makedirs(reportFolder)
+
+# train the classifiers
+for clf in classifiers:
+    clf.fit(dbtrain, ytrain)
+
+    ytestFull = []
+    ypredFull = []
+
+    # for each year in the data
+    for year in data["year"].unique():
+        # select the data of the year
+        dbtest=data.loc[data["year"]==year].copy()
+        # drop the year column
+        dbtest.drop("year", axis=1, inplace=True)
+        # extract the y column
+        ytest=dbtest["Y"]
+        # drop the y column
+        dbtest.drop("Y", axis=1, inplace=True)
+
+        # predict the y values
+        ypred=clf.predict(dbtest)
+
+        # save the y values and the predicted y values if the year isn't in the training data
+        if year not in year_train:
+            ytestFull.extend(ytest)
+            ypredFull.extend(ypred)
+
+        # save the report
+        reportString = "Report for year " + year + "\n"
+        reportString += "Confusion matrix\n"
+        reportString += str(confusion_matrix(ytest, ypred)) + "\n"
+        reportString += "Normalized confusion matrix\n"
+        reportString += str(confusion_matrix(ytest, ypred, normalize='true')) + "\n"
+        reportString += "Accuracy\n"
+        reportString += str(accuracy_score(ytest, ypred)) + "\n"
+        reportString += "Classification report\n"
+        reportString += str(classification_report(ytest, ypred)) + "\n"
+
+        # save the report to a file
+        reportFile = reportFolder + clf_labels[classifiers.index(clf)] + year + ".txt"
+        with open(reportFile, 'w') as f:
+            f.write(reportString)
+
+    # Save the report for all years
+    reportString = "Report for all years\n"
+    reportString += "Confusion matrix\n"
+    reportString += str(confusion_matrix(ytestFull, ypredFull)) + "\n"
+    reportString += "Normalized confusion matrix\n"
+    reportString += str(confusion_matrix(ytestFull, ypredFull, normalize='true')) + "\n"
+    reportString += "Accuracy\n"
+    reportString += str(accuracy_score(ytestFull, ypredFull)) + "\n"
+    reportString += "Classification report\n"
+    reportString += str(classification_report(ytestFull, ypredFull)) + "\n"
+
+    if accuracy_score(ytestFull, ypredFull)>bestacc:
+        bestacc=accuracy_score(ytestFull, ypredFull)
+        bestclf=clf_labels[classifiers.index(clf)]
+    
+    accs.append(accuracy_score(ytestFull, ypredFull))
+
+    # save the report to a file
+    reportFile = reportFolder + clf_labels[classifiers.index(clf)] + ".txt"
+    with open(reportFile, 'w') as f:
+        f.write(reportString)
+    #print the classifier and the accuracy
+    print(clf_labels[classifiers.index(clf)], accuracy_score(ytestFull, ypredFull))
+
+# save the accuracies to a file
+reportFile = reportFolder + "accuracies.csv"
+with open(reportFile, 'w') as f:
+    f.write("Classifier, Accuracy\n")
+    for i in range(len(accs)):
+        f.write(clf_labels[i] + ", " + str(accs[i]) + "\n")
+
+print("Best accuracy: ", bestacc)
+print("Best classifier: ", bestclf)
