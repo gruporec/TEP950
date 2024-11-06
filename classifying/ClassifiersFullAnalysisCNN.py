6442625ad1097fb7439dc30fceff57c6963b18f6
GEPOC <webgepoc@gmail.com>
2024-11-06 18:25:16 +0100

    merge

--- classifying/ClassifiersFullAnalysisCNN.py
+++ classifying/ClassifiersFullAnalysisCNN.py
@@ -72,10 +72,10 @@ class CNNHypermodel(kt.HyperModel):
 
 # Databases
 #ZIM 'raw','db/ZIMdb14151619raw.csv',
-# dataFiles=['db/ZIMdb14151619ZIM40MET0.csv','db/ZIMdb14151619ZIM80MET0.csv','db/ZIMdb14151619ZIM120MET0.csv','db/ZIMdb14151619ZIM40MET4.csv','db/ZIMdb14151619ZIM80MET4.csv','db/ZIMdb14151619ZIM120MET4.csv','db/ZIMdb14151619ZIM40MET8.csv','db/ZIMdb14151619ZIM80MET8.csv','db/ZIMdb14151619ZIM120MET8.csv']
-# dataLabels=['40Z0M','80Z0M','120Z0M','40Z4M','80Z4M','120Z4M','40Z8M','80Z8M','120Z8M']
-dataFiles=['db/ZIMdb14151619oldIRNAS.csv']
-dataLabels=['ZIMindicators']
+dataFiles=['db/ZIMdb14151619ZIM40MET0.csv','db/ZIMdb14151619ZIM80MET0.csv','db/ZIMdb14151619ZIM120MET0.csv','db/ZIMdb14151619ZIM40MET4.csv','db/ZIMdb14151619ZIM80MET4.csv','db/ZIMdb14151619ZIM120MET4.csv','db/ZIMdb14151619ZIM40MET8.csv','db/ZIMdb14151619ZIM80MET8.csv','db/ZIMdb14151619ZIM120MET8.csv']
+dataLabels=['40Z0M','80Z0M','120Z0M','40Z4M','80Z4M','120Z4M','40Z8M','80Z8M','120Z8M']
+# dataFiles=['db/ZIMdb14151619oldIRNAS.csv']
+# dataLabels=['ZIMindicators']
 #TDV
 # dataFiles=['db/TDVdb14151619raw.csv', 'db/TDVdb14151619meteoraw.csv']
 # #dataFiles=['db/TDVdb14151619meteoraw.csv']
@@ -99,7 +99,7 @@ verbose=True
 confmat=True
 
 # Report file in results/ZIM/ of root directory, the name of the file is the name of the classifier
-reportFolder='results/ZIM/AnalisysOldIRNAS2/'
+reportFolder='results/ZIM/Analysis 1619/'
 
 # years to be used as training data
 #years_train=[['2014'], ['2015'], ['2016'], ['2019']]
